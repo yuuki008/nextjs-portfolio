595521722cf62acc411350d09b716ebbeed6b31c
yuuki008 <s22ah261@seinan-gakuin.jp>
2021-01-24 01:32:03 +0900

    [fix] タイトル変更

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,26 @@
 import App from "next/app";
 import Head from "next/head";
-
+import "../public/styled.css";
 
 export default class MyApp extends App {
-  render(){
-    const {Component, pageProps} = this.props
-    return(
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
       <>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, minimum-scale=1, maxmum-scale=1"
-        />
-        <title>nomus portfolio</title>
-      </Head>
-      <Component {...pageProps} />
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,700;1,400&display=swap"
+            rel="stylesheet"
+          />
+          <meta
+            name="viewport"
+            content="width=device-width, minimum-scale=1, maxmum-scale=1"
+          />
+          <title>Yuuki Nomura</title>
+        </Head>
+        <Component {...pageProps} />
       </>
-    )
+    );
   }
 }
